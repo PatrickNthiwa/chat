3d9e1b44b0e863def545b0106bf65f8372135db2
PatrickNthiwa <pwnthiwa@gmail.com>
2023-02-02 08:27:42 +0300

    Updated routes and the home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-md-5">
+            @if($users->count() > 0)
+                <h3>Pick a user to chat with</h3>
+                <ul id="users">
+                    @foreach($users as $user)
+                        <li><span class="label label-info">{{ $user->name }}</span> <a href="javascript:void(0);" class="chat-toggle" data-id="{{ $user->id }}" data-user="{{ $user->name }}">Open chat</a></li>
+                    @endforeach
+                </ul>
+            @else
+                <p>No users found! try to add a new user using another browser by going to <a href="{{ url('register') }}">Register page</a></p>
+            @endif
         </div>
     </div>
-</div>
-@endsection
+
+    @include('chat-box')
+
+    <input type="hidden" id="current_user" value="{{ \Auth::user()->id }}" />
+    <input type="hidden" id="pusher_app_key" value="{{ env('PUSHER_APP_KEY') }}" />
+    <input type="hidden" id="pusher_cluster" value="{{ env('PUSHER_APP_CLUSTER') }}" />
+@stop
+
+@section('script')
+    <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
+    <script src="{{ asset('js/chat.js') }}"></script>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
